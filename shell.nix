3dd0625aca25aaa2e2a2c2a5b5be36bbf16fff3a
Thomas Reichert <thomasr1987@gmail.com>
2026-02-18 08:29:27 +1100

    Replace deprecated xorg dependencies in nix shell

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ mkShell rec {
   ];
   buildInputs = [
     udev alsa-lib-with-plugins vulkan-loader
-    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+    libx11 libxcursor libxi libxrandr # To use the x11 feature
     libxkbcommon wayland # To use the wayland feature
   ];
   LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
